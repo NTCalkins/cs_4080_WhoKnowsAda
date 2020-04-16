d18d7e9a09106e94590f11d964575e45c6b285ea
hlin91 <harveylin@cpp.edu>
2020-04-16 09:30:46 -0700

    Removed redundant files.

--- DynamicString.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "DynamicString.c"
-
-struct DynamicString;
-void DynamicString_clear(struct DynamicString*);
-void DynamicString_append(struct DynamicString*, char*);
-void DynamicString_copy(struct DynamicString*, char*);
-struct DynamicString makeString();
-inline void deleteString(struct DynamicString*);
--- StaticLinkNode.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "StaticLinkNode.c"
-
-struct Node;
-void Node_copy(struct Node*, char*); /* Copy contents of string into node */
-void Node_append(struct Node*, char*); /* Append contents of string into node */
-char* Node_get(struct Node*); /* Get pointer to string contents of node */
-struct Node* makeNode(); /* Construct a Node */
-void deleteNode(struct Node*); /* Clean up a Node */
