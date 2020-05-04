76e56daa043871e931d53cd53e18137639e8a9b2
NTCalkins <NicholasTCalkins@gmail.com>
2020-05-03 19:33:17 -0700

    added test and testfiles

---
+++ Scala/StaticLengthCodeAndTests/testStaticMemoryTime.py
@@ -0,0 +1,10 @@
+import os
+import time
+
+
+start = time.time()
+
+for x in range(32):
+    start = time.time()
+    os.system("scala StaticBufferCommands < bigbible_append_then_write.txt")
+    print(time.time() - start)
