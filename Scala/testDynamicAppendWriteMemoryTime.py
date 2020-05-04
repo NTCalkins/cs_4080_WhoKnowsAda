465d5f29635057998225806592f862e9cadda48d
NTCalkins <NicholasTCalkins@gmail.com>
2020-05-03 20:24:03 -0700

    began writing test to test the difference in joining

--- Scala/testDynamicAppendWriteMemoryTime.py
+++ Scala/testDynamicAppendWriteMemoryTime.py
@@ -8,3 +8,5 @@ for x in range(32):
     start = time.time()
     os.system("scala DynamicBufferCommands < TestFiles/bigbible_append_then_write.txt")
     print(str(time.time() - start) + " seconds")
+
+os.system("rm bigbibleoutput.txt")
