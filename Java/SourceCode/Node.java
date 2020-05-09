3ebbe355787521478136057be2d2ac14af9b04eb
NTCalkins <NicholasTCalkins@gmail.com>
2020-05-09 14:55:21 -0700

    wrote tests for Java code, renamed directory

---
+++ Java/SourceCode/Node.java
@@ -0,0 +1,6 @@
+public abstract class Node
+{
+    public abstract void copy(String s);
+    public abstract void append(String s);
+    public abstract String get();
+}
--- WhoKnowsJava/SourceCode/Node.java
+++
@@ -1,6 +0,0 @@
-public abstract class Node
-{
-    public abstract void copy(String s);
-    public abstract void append(String s);
-    public abstract String get();
-}
